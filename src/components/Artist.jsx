2c577b4e0d78957456e7cd8241e1d3dd2b3bd5d9
Wajeeh u Hassan <m.wajeeh.qureshi@gmail.com>
2023-09-01 02:55:24 +0500

    App.js file deleted

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -5,7 +5,7 @@ const Artist = (props) => {
     console.log(props?.artist?.name)
     return (
         <Col span={6}>
-            <div style={{marginLeft:'auto',marginTop:'auto'}}>
+            <div>
                 <a href='https://www.w3schools.com/howto/howto_css_rounded_images.asp'>
                     <Image preview={false} style={{ borderRadius: '70%',margin:'auto' }} src={props?.artist?.images[2]?.url} />
                     <p style={{ textAlign: 'center' }}>{props?.artist?.name}</p>
