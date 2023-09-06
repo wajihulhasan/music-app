03f89b54bda522ecfbaccef0c610d0080fdfed6e
Wajeeh u Hassan <m.wajeeh.qureshi@gmail.com>
2023-09-07 03:56:47 +0500

    spotify player using apis

--- src/components/Musicbar.jsx
+++ src/components/Musicbar.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
@@ -5,17 +6,38 @@ import 'react-h5-audio-player/lib/styles.css';
 
 const Musicbar = () => {
 
+    const song_url = 'https://api.spotify.com/v1/me/player/currently-playing';
+    const playtrack = async () => {
+        const response = await axios.put(`${song_url}`,
+            {
 
+                context_uri: "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
+                offset: {
+                    position: 5
+                },
+                position_ms: 0
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('user_token')}`,
+                    'Content-Type': 'application/json'
+                }
+            })
+        if (response.status === 200) {
+            console.log()
+        }
+        console.log(response)
+    }
     return (
         <div>
             <AudioPlayer
-                autoPlay
-                src="http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3"
-                onPlay={e => console.log("onPlay")}
+                //autoPlay
+                src="http://example.com/audio.mp3"
+                onPlay={e =>{ console.log("onPlay")}}
             // other props here
             />
         </div>
     )
-} 
+}
 
 export default Musicbar
--- src/components/WebPlayback.jsx
+++ src/components/WebPlayback.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-const playback_url="";
+const playback_url = "";
 
 function WebPlayback(props) {
 
-    const playback_url='https://api.spotify.com/v1/me/player'
-    
-    return(
+    const [object_id, setObject_id] = useState()
+
+    const playback_url = 'https://api.spotify.com/v1/me/player'
+
+    const get_user_playback = async () => {
+        const response = await axios.get(`${playback_url}`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('user_token')}`
+            }
+        })
+        if (response.status === 200) {
+            console.log(response.data.device.id)
+            let deviceInfo = response.data.device;
+            setObject_id(deviceInfo.id);
+            localStorage.setItem('device_id',`${deviceInfo.id}`)
+        }
+    }
+
+    useEffect(() => {
+        get_user_playback()
+    }, []);
+    useEffect(() => {
+
+    }, [object_id]);
+    return (
         <></>
     )
 }
